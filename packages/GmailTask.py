e016b450c2f04edcede919edc10f752c165be7a5
Dương Võ Thiên Bảo <135791389+Afihu@users.noreply.github.com>
2023-10-30 15:45:48 +0700

    Modify: GmailTask

--- packages/GmailTask.py
+++ packages/GmailTask.py
@@ -5,6 +5,7 @@ import base64
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os.path
+import time
 
 class GmailTask:
     def __init__(self):
@@ -17,21 +18,23 @@ class GmailTask:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'])
+                    'packages\Credentials.json', ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'])
                 creds = flow.run_local_server(port=0)
             with open('token.json', 'w') as token:
                 token.write(creds.to_json())
         self.service = build('gmail', 'v1', credentials=creds)
 
-    # Rest of your GmailTask methods...
-
     def GmailTask_Run(self):
+        # Get the recipient's email address, subject and email body
+        recipient_email = input("Enter the recipient's email address: ")
+        subject=input("Your sunject: ")
+        body=input("Your email body: \n")
         # Create a simple email message
-        msg = MIMEText("This is the body of the email.")
-        msg['Subject'] = "This is the subject of the email."
+        msg = MIMEText(body)
+        msg['Subject'] = subject
         msg['From'] = "your-email@gmail.com"
-        msg['To'] = "recipient-email@gmail.com"
-        
+        msg['To'] = recipient_email
+
         # Encode the message and send it
         encoded_msg = base64.urlsafe_b64encode(msg.as_bytes()).decode()
         message = self.service.users().messages().send(userId="me", body={"raw": encoded_msg}).execute()
@@ -58,3 +61,24 @@ class GmailTask:
                 print('From: ', sender)
             except Exception as e:
                 print('An error occurred: %s' % e)
+
+if __name__ == "__main__":
+    gmail_task = GmailTask()
+    while True:
+        print("1. Check Inbox")
+        print("2. Compose Email")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Refresh Inbox
+            gmail_task.GmailTask_CheckInbox()# assuming the CheckInbox task is at index 0
+        elif choice == '2':
+            # Compose Email
+            gmail_task.GmailTask_Run() # assuming the Run task is at index 1
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+    time.sleep(0.5)
